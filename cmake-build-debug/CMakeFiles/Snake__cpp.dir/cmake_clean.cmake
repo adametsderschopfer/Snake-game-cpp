7959591252b45d379a975f8ce3bc3106436fd9ae
Adamets Vladislav <v.adamets@consult-info.ru>
2021-03-20 00:45:57 +0200

    Config CLion files

---
+++ cmake-build-debug/CMakeFiles/Snake__cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snake__cpp.dir/Game.cpp.obj"
+  "Snake__cpp.exe"
+  "Snake__cpp.exe.manifest"
+  "Snake__cpp.pdb"
+  "libSnake__cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Snake__cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
